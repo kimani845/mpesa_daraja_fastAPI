6c198ccc5014a059a860f0fc7e5abd095d934554
Bushbaby <kimanimichael09@gmail.com>
2025-10-22 14:33:12 +0300

    add http client wrapper

--- app/http_client/http.py
+++ app/http_client/http.py
@@ -1,57 +1,115 @@
-import _asyncio
-from socket import AF_INET, SOCK_STREAM
+import asyncio
+
+from socket import AF_INET
 from typing import Optional, Any
 
 import aiohttp
 
-SIZE_POOL_AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(
-    total=30,
-    connect=10,
-    sock_connect=10,
-    sock_read=10,
-)
+
+SIZE_POOL_AIOHTTP = 200
+
 
 class Http:
-    sem: Optional[_asyncio.Semophore] = None
-    aiohttp_client_session: Optional[aiohttp.ClientSession] = None
-    connector: Optional[aiohttp.TCPConnector] = None
+    sem: Optional[asyncio.Semaphore] = None
+    aiohttp_client: Optional[aiohttp.ClientSession] = None
 
     @classmethod
-    def get_aiohttp_client_session(cls) -> aiohttp.ClientSession:
-        if cls.aiohttp_client_session is None:
-            if cls.connector is None:
-                cls.connector = aiohttp.TCPConnector(
-                    limit=0,
-                    limit_per_host=0,
-                    family=AF_INET,
-                    ssl=False,
-                )
-            cls.aiohttp_client_session = aiohttp.ClientSession(
-                connector=cls.connector,
-                timeout=SIZE_POOL_AIOHTTP_TIMEOUT,
-            )
-        return cls.aiohttp_client_session
-    
-    @classmethod
-    async def close_aiohttp_client_session(cls) -> None:
-        if cls.aiohttp_client_session is not None:
-            await cls.aiohttp_client_session.close()
-            cls.aiohttp_client_session = None
-        if cls.connector is not None:
-            await cls.connector.close()
-            cls.connector = None    
+    def get_aiohttp_client(cls) -> aiohttp.ClientSession:
+        if cls.aiohttp_client is None:
+            timeout = aiohttp.ClientTimeout(total=50)
+            connector = aiohttp.TCPConnector(family=AF_INET, limit_per_host=SIZE_POOL_AIOHTTP)
+            cls.aiohttp_client = aiohttp.ClientSession(timeout=timeout, connector=connector)
+
+        return cls.aiohttp_client
 
     @classmethod
-    async def get(cls, url : str, password : str, headers : Optiotional[dict[str, str]] = None) -> Any:
-        client_session = cls.get_aiohttp_client_session()
+    async def close_aiohttp_client(cls) -> None:
+        if cls.aiohttp_client:
+            await cls.aiohttp_client.close()
+            cls.aiohttp_client = None
+
+    @classmethod
+    async def get(cls, url: str, username: str, password: str, headers: Optional[dict] = None,
+                  data: Optional[dict] = None) -> Any:
+        client = cls.get_aiohttp_client()
         try:
-            async with client_session.get(
-                url,
-                headers=headers,
-                auth=aiohttp.BasicAuth(login='', password=password),
-            ) as response:
-                response.raise_for_status()
-                return await response.json()
+
+            async with client.get(url, headers=headers, data=data,
+                                  auth=aiohttp.BasicAuth(username, password)) as response:
+                if response.status != 200:
+                    _response = await response.text()
+                    payload = {"response": _response,
+                               "url": url,
+                               "http_method": "get",
+                               "headers": headers,
+                               "response_status": f"{response.status}, {response.reason}",
+                               "success": False}
+
+                    return payload
+
+                json_result = await response.json()
+        except aiohttp.ClientConnectorError:
+            payload = {"error": "Cannot connect to host.",
+                       "url": url,
+                       "http_method": "get",
+                       "headers": headers,
+                       "success": False}
+
+            return payload
         except Exception as e:
-            raise e
-            
+            payload = {"error": str(e),
+                       "url": url,
+                       "http_method": "get",
+                       "headers": headers,
+                       "response_status": f"{response.status}, {response.reason}",
+                       "success": False}
+
+            return payload
+
+        return json_result
+
+    @classmethod
+    async def post(cls, url: str, headers: Optional[dict] = None, data: Optional[str] = None) -> Any:
+        client = cls.get_aiohttp_client()
+
+        try:
+            async with client.post(url, headers=headers, data=data) as response:
+                if response.status != 200:
+                    _response = await response.text()
+                    payload = {"response": _response,
+                               "url": url,
+                               "http_method": "postString",
+                               "headers": headers,
+                               "response_status": f"{response.status}, {response.reason}",
+                               "success": False}
+
+                    return payload
+
+                json_result = await response.json()
+        except aiohttp.ClientConnectorError:
+            payload = {"error": "Cannot connect to host",
+                       "url": url,
+                       "http_method": "post",
+                       "headers": headers,
+                       "success": False}
+
+            return payload
+        except Exception as e:
+            payload = {"error": str(e),
+                       "url": url,
+                       "http_method": "post",
+                       "headers": headers,
+                       "response_status": f"{response.status}, {response.reason}",
+                       "success": False}
+
+            return payload
+
+        return json_result
+
+
+async def on_start_up() -> None:
+    Http.get_aiohttp_client()
+
+
+async def on_shutdown() -> None:
+    await Http.close_aiohttp_client()
